6ba1d66b5b5f85d547ae959e963464bd85fc20e0
Helmuth Sodetz <sodhel2001@gmail.com>
2023-09-19 19:49:50 -0500

    aplicando tema dark a la app

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,8 +1,11 @@
+
 import 'package:flutter/material.dart';
 
 class AppTheme {
+
   
   ThemeData getTheme() => ThemeData(
+    brightness: Brightness.dark,
     useMaterial3: true,
     colorSchemeSeed: const Color(0xFF2862F5),
   );
